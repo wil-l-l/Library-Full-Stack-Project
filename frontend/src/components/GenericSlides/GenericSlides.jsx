7a5f7dae5a3d3acf3eba3fcedf8daee08ac4314f
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-28 11:50:02 -0400

    Add css and html to 'GenericSlides' component

--- frontend/src/components/GenericSlides/GenericSlides.jsx
+++ frontend/src/components/GenericSlides/GenericSlides.jsx
@@ -1,4 +1,7 @@
 import "./GenericSlides.css";
+import HeadphonesIcon from "../../assets/icons/headphones.png";
+import EBookIcon from "../../assets/icons/ebook.png";
+import ComicIcon from "../../assets/icons/comic.png";
 
 const GenericSlides = ({ title }) => {
   return (
@@ -8,7 +11,23 @@ const GenericSlides = ({ title }) => {
         <p className="underline-on-hover">See More</p>
       </div>
       <ul className="generic-slides-box__list">
-        <li className="generic-slides-box__list__item"></li>
+        <li className="generic-slides-box__list__item">
+          <div className="generic-slides-box__list__item__cover-img-box">
+            <img src="" alt="" />
+          </div>
+          <div className="generic-slides-box__list__item__book-details">
+            <div className="generic-slides-box__list__item__book-details__book-type-box">
+              <img
+                src={EBookIcon}
+                alt=""
+                className="generic-slides-box__list__item__book-details__book-type-box__icon"
+              />
+              <p>Ebook</p>
+            </div>
+            <p>What Every Body Is Saying</p>
+            <p>Joe Navarro</p>
+          </div>
+        </li>
         <li className="generic-slides-box__list__item"></li>
         <li className="generic-slides-box__list__item"></li>
       </ul>
