4c32de01f78da51470de738b50a98a4a91c5425d
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-29 15:30:43 -0400

    Add sever proxy to vite config settings and replace 'isLoading' state with 'loadedBooks' state in the 'HomePage' component

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,28 @@ import NavBar from "../../components/NavBar/NavBar";
 import TrendingSlides from "../../components/TrendingSlides/TrendingSlides";
 import GenericSlides from "../../components/GenericSlides/GenericSlides";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function HomePage() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [loadedBooks, setLoadedBooks] = useState(null);
 
-  return isLoading ? (
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      const response = await fetch("/api/books");
+      const books = await response.json();
+      if (books.success) {
+        setLoadedBooks(books);
+      } else {
+        setLoadedBooks(false);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return loadedBooks === null ? (
     <p>Application is loading...</p>
+  ) : loadedBooks === false ? (
+    <p className="red-text">Could not load data</p>
   ) : (
     <>
       <NavBar />
