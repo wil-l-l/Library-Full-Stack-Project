0bcf2ab98053b7dac2ecee03d92935d93966bdb4
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-30 11:56:45 -0400

    Begin styling the search bar menu that opens when the search icon is clicked

--- frontend/src/components/NavBar/NavBarItem/NavBarItem.jsx
+++ frontend/src/components/NavBar/NavBarItem/NavBarItem.jsx
@@ -1,18 +1,39 @@
-import { useNavigate } from "react-router";
+import { useState } from "react";
+import "./NavBarItem.css";
 
 const NavBarItem = ({ icon, label }) => {
-  const navigate = useNavigate();
+  const [active, setActive] = useState(false);
 
   return (
-    <button
-      className="nav-bar__option-btn"
-      onClick={() => navigate(`/${label.toLowerCase()}`)}
-    >
-      <div className="nav-bar__option-btn__img-box">
-        <img src={icon} alt="" className="nav-bar__option-btn__img-box__img" />
-      </div>
-      <p>{label}</p>
-    </button>
+    <>
+      {active && (
+        <div className="nav-bar__search-menu">
+          <p>Search</p>
+          <form action="">
+            <input
+              type="text"
+              name=""
+              id=""
+              className="nav-bar__search-menu__input"
+              placeholder="Search"
+            />
+          </form>
+        </div>
+      )}
+      <button
+        className={`nav-bar__option-btn`}
+        onClick={() => setActive(!active)}
+      >
+        <div className="nav-bar__option-btn__img-box">
+          <img
+            src={icon}
+            alt=""
+            className="nav-bar__option-btn__img-box__img"
+          />
+        </div>
+        <p>{label}</p>
+      </button>
+    </>
   );
 };
 
