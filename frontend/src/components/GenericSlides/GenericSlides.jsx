95bb3329bb759772ed92f0494c756d2c1fca6620
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-30 18:51:47 -0400

    Add onClickHandler to the 'See More' links that navigates to the search page of the genre selected

--- frontend/src/components/GenericSlides/GenericSlides.jsx
+++ frontend/src/components/GenericSlides/GenericSlides.jsx
@@ -6,15 +6,27 @@ import GenericSlideItem from "../GenericSlideItem/GenericSlideItem";
 import { useContext } from "react";
 import { BooksContext } from "../../contexts/BooksContext";
 import sharedConstants from "../../../../sharedConstants";
+import { useNavigate } from "react-router";
 
 const GenericSlides = ({ categoryTitle }) => {
   const { books } = useContext(BooksContext);
+  const navigate = useNavigate();
 
   return (
     <div className="generic-slides-box">
       <div className="generic-slides-box__text-box page-padding">
         <h2>{categoryTitle}</h2>
-        <p className="underline-on-hover">See More</p>
+        <p
+          className="underline-on-hover"
+          onClick={() => {
+            for (const key in sharedConstants.mediaTypes) {
+              if (categoryTitle.match(new RegExp(key, "gi")))
+                navigate(`/search?genre=${key}`);
+            }
+          }}
+        >
+          See More
+        </p>
       </div>
       <ul className="generic-slides-box__list horizontal-scroll-box">
         {books
