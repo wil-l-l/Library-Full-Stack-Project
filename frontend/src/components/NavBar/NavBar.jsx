bbb35fe1bea993e71f4e172add28af12cb60d27e
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-30 15:48:52 -0400

    Made the welcome text in the 'NavBar' redirect back to the home page

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,11 +3,14 @@ import NavBarItem from "./NavBarItem/NavBarItem";
 import HomeIcon from "../../assets/icons/home.svg";
 import SearchIcon from "../../assets/icons/search--white.png";
 import ProfileIcon from "../../assets/icons/profile.png";
+import { useNavigate } from "react-router";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="nav-bar">
-      <div className="nav-bar__text-box">
+      <div className="nav-bar__text-box" onClick={() => navigate(`/`)}>
         <p className="nav-bar__text-box__welcome-txt">Welcome to</p>
         <p className="nav-bar__text-box__library-txt">E-Library</p>
       </div>
