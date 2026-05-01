672b6d6248cda5a6e76ae6c5f9b6ee0b925a2fb4
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-01 08:59:11 -0400

    Do fetch request to create new user in signup page

--- frontend/src/pages/SignupPage/SignupPage.jsx
+++ frontend/src/pages/SignupPage/SignupPage.jsx
@@ -1,10 +1,37 @@
+import { useRef } from "react";
 import "./SignupPage.css";
 import { Link } from "react-router";
 
 const SignupPage = () => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
   return (
     <div className="signup-page">
-      <form action="" className="signup-form">
+      <form
+        action=""
+        className="signup-form"
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          let response = await fetch("/api/signup", {
+            method: "POST",
+            body: JSON.stringify({
+              username: usernameRef.current.value,
+              password: passwordRef.current.value,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          response = await response.json();
+          console.log(response);
+
+          usernameRef.current.value = "";
+          passwordRef.current.value = "";
+        }}
+      >
         <h2 className="signup-page__main-heading">Sign up</h2>
         <p className="signup-page__features-text">
           Sign up to borrow books online!
@@ -13,11 +40,13 @@ const SignupPage = () => {
           type="text"
           placeholder="Username"
           className="signup-form__input-field"
+          ref={usernameRef}
         />
         <input
           type="password"
           placeholder="Password"
           className="signup-form__input-field"
+          ref={passwordRef}
         />
         <button type="submit" className="signup-page__submit-btn">
           Sign up
