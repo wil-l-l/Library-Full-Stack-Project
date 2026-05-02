63c400453b31ed7ef84cf2b3f30169f6e9d2b871
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-02 17:34:54 -0400

    Remove ProfilePage component from project

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router";
 
 import HomePage from "./pages/HomePage/HomePage";
 import SearchPage from "./pages/SearchPage/SearchPage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import MediaPage from "./pages/MediaPage/MediaPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -15,7 +14,6 @@ const router = createBrowserRouter([
     path: "/home",
   },
   { element: <SearchPage />, path: "/search" },
-  { element: <ProfilePage />, path: "/profile" },
   { element: <MediaPage />, path: "/book/:id" },
   { element: <SignupPage />, path: "/signup" },
   { element: <LoginPage />, path: "/login" },
