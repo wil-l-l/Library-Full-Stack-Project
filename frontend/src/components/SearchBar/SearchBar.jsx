7aa894e21ef777b3d76be9096cac9ee7778d26c4
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-03 17:20:43 -0400

    Add onSubmitHandler back to SearchBar

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,34 @@
 import "./SearchBar.css";
 import SearchIcon from "../../assets/icons/search--black.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const SearchBar = () => {
+  const [searchText, setSearchText] = useState("");
+  const navigate = useNavigate();
+
   return (
     <div className="search-bar">
       <img src={SearchIcon} alt="" className="search-bar__img" />
-      <input
-        type="text"
-        className="search-bar__input"
-        placeholder="Search by book or author"
-      />
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          navigate(`/search`, {
+            state: {
+              searchText,
+            },
+          });
+          setSearchText("");
+        }}
+      >
+        <input
+          type="text"
+          className="search-bar__input"
+          placeholder="Search by book or author"
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
