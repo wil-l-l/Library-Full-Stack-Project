d5279f579b492d2ce83dc8c2628f1d514b8b23b1
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-04 16:29:01 -0400

    Editing the validation in login route to find the right password with bcrypt

--- backend/routes/login.route.js
+++ backend/routes/login.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { User, validateUser } = require("../models/user.model");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
@@ -12,8 +14,8 @@ router.post("/", async (req, res) => {
       message: "Incorrect username or password",
     });
 
-  let userFound = await User.findOne({ username, password }).select(
-    "username books favorites history",
+  let userFound = await User.findOne({ username }).select(
+    "username password books favorites history",
   );
 
   if (!userFound)
@@ -21,10 +23,16 @@ router.post("/", async (req, res) => {
       .status(404)
       .send({ success: false, message: "Incorrect username or password" });
 
+  const matchedPassword = await bcrypt.compare(password, userFound.password);
+  if (!matchedPassword)
+    return res
+      .status(404)
+      .send({ success: false, message: "Incorrect username or password" });
+
   res.status(200).send({
     success: true,
     message: "Successfully logged in.",
-    data: userFound,
+    data: { ...userFound._doc, password: undefined },
   });
 });
 
