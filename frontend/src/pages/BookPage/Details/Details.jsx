765de2784f5898b06b3c3c8b4d4e00f45282c078
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-04 09:11:41 -0400

    Add a breakline to book summary

--- frontend/src/pages/BookPage/Details/Details.jsx
+++ frontend/src/pages/BookPage/Details/Details.jsx
@@ -5,10 +5,28 @@ import { useNavigate } from "react-router";
 import formatAuthorName from "../../../utils/formatAuthorName";
 import PlusIcon from "../../../assets/icons/plus.png";
 import MinusIcon from "../../../assets/icons/minus.png";
+import sharedConstants from "../../../../../sharedConstants";
 
 const Details = ({ summary, tags, authors }) => {
   const [doExpandText, setDoExpandText] = useState(false);
   const navigate = useNavigate();
+
+  const getSummaryByParts = () => {
+    const regex = /--break/gi;
+    const match = regex.exec(sharedConstants.bookSummary);
+    return (
+      <>
+        <p>{sharedConstants.bookSummary.substring(0, match.index)}</p>
+        <br />
+        <p>
+          {sharedConstants.bookSummary.substring(
+            match.index + 7,
+            sharedConstants.bookSummary.length,
+          )}
+        </p>
+      </>
+    );
+  };
 
   return (
     <div className="book-page__details">
@@ -16,7 +34,7 @@ const Details = ({ summary, tags, authors }) => {
         heading={"About this Book"}
         content={
           <>
-            <p
+            <div
               style={
                 !doExpandText
                   ? {
@@ -29,10 +47,8 @@ const Details = ({ summary, tags, authors }) => {
                   : {}
               }
             >
-              {
-                "Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis dis parturient montes nascetur ridiculus mus donec rhoncus eros lobortis nulla molestie mattis scelerisque maximus eget fermentum odio phasellus non purus est efficitur laoreet mauris pharetra vestibulum fusce dictum risus. Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis dis parturient montes nascetur ridiculus mus donec rhoncus eros lobortis nulla molestie mattis scelerisque maximus eget fermentum odio phasellus non purus est efficitur laoreet mauris pharetra vestibulum fusce dictum risus."
-              }
-            </p>
+              {getSummaryByParts()}
+            </div>
             <button
               className="book-page__interact-btn"
               style={{ zoom: ".8", fontWeight: "600px", marginTop: "10px" }}
