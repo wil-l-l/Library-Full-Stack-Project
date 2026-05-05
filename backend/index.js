da78fd49ceaf1bbf04d349c3fe6e7db4f247c80d
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-05 11:55:45 -0400

    Add mongoDB connection string as a private environment variable

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 process.on("uncaughtException", (ex) => {
-  console.error("Caught an Uncaught exception");
+  console.error("Caught an Uncaught exception", ex);
   process.exit(1);
 });
 
@@ -18,11 +18,16 @@ if (!config.get("jwtPrivateKey")) {
   process.exit(1);
 }
 
+if (!config.get("db")) {
+  console.error("Could not start app.\nError: db is not defined");
+  process.exit(1);
+}
+
 require("./startup/routes")(app);
 require("./startup/production")(app);
 
 mongoose
-  .connect("mongodb://localhost/library")
+  .connect(config.get("db"))
   .then(() => console.log("Connected to mongodb"))
   .catch((err) => console.log("Could not connect to mongodb: ", err.message));
 
