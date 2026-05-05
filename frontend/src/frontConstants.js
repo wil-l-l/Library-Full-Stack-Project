40bab2a6c13de68bc4b5d4cd0b99c3bcf9342fc2
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-05 15:31:06 -0400

    Fix the export of frontConstants

--- frontend/src/frontConstants.js
+++ frontend/src/frontConstants.js
@@ -1,13 +1,13 @@
-const sharedConstants = {};
+const frontConstants = {};
 
-sharedConstants.mediaTypes = {
+frontConstants.mediaTypes = {
   ebook: "Ebook",
   audiobook: "Audiobook",
 };
 
-sharedConstants.maxLoanLimit = 2;
+frontConstants.maxLoanLimit = 2;
 
-sharedConstants.bookSummary =
+frontConstants.bookSummary =
   "Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis dis parturient montes nascetur ridiculus mus donec rhoncus eros lobortis nulla molestie mattis scelerisque maximus eget fermentum odio phasellus non purus est efficitur laoreet mauris pharetra vestibulum fusce dictum risus. --breakLorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis dis parturient montes nascetur ridiculus mus donec rhoncus eros lobortis nulla molestie mattis scelerisque maximus eget fermentum odio phasellus non purus est efficitur laoreet mauris pharetra vestibulum fusce dictum risus.";
 
-export default sharedConstants;
+export default frontConstants;
